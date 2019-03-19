aa5b1de126711e69a261ca8c21abccd8c5273fdb
mktrptn <mktr@gmail.com>
2019-03-19 18:08:28 +0530

    initial

---
+++ mvccoreapplicatio/mvccoreapplicatio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvccoreapplicatio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
