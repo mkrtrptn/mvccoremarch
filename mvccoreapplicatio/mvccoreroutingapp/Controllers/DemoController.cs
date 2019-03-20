64adfe9cb6ce17bfc2e2b56ad6b5dd1b3e62a304
mktrptn <mktr@gmail.com>
2019-03-20 21:25:50 +0530

    intial commit

---
+++ mvccoreapplicatio/mvccoreroutingapp/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvccoreroutingapp.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getinfobyid(int id)
+        {
+            ViewBag.info = id + "Product";
+            return View();
+        }
+
+        public ActionResult getinfobyidname(int id , int name)
+        {
+            ViewBag.info = id + name + "Product";
+            return View();
+        }
+
+
+    }
+}
---
+++ mvccoreapplicatio/mvcvsmvccore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcvsmvccore.MvcApplication" Language="C#" %>
